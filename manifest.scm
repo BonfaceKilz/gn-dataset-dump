191940a088cfbd957848f91c0c652a8996c499e2
Munyoki Kilyungi <me@bonfacemunyoki.com>
2023-01-19 15:15:02 +0300

    Define guix dependencies for this project
    * manifest.scm: New file.

---
+++ manifest.scm
@@ -0,0 +1,14 @@
+(specifications->manifest
+ (list "sbcl"
+       "sbcl-alexandria"
+       "sbcl-arrows"
+       "sbcl-cl-fad"
+       "sbcl-cl-json"
+       "sbcl-cl-str"
+       "sbcl-dbi"
+       "sbcl-ironclad"
+       "sbcl-listopia"
+       "sbcl-lmdb"
+       "sbcl-slynk"
+       "sbcl-trivia"
+       "sbcl-trivial-utf-8"))
